383e024b41636057bd030b70b0cf3ed89f99dd65
Farrel Muhammad <farrelf11@gmail.com>
2022-12-28 18:14:45 +0700

    Update Navbar

--- .history/src/App_20221228172858.js
+++ .history/src/App_20221228172858.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <Home />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   );
 }
---
+++ .history/src/components/Navbar_20221228173501.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Logo from "../assets/Logo.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-white border-blue-500 dark:bg-blue-500">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+          <a href="https://flowbite.com" className="flex items-center">
+            <img
+              src={Logo}
+              className="h-2 mr-3 sm:h-9"
+              alt="Arkademi Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex items-center">
+            <a
+              href="tel:5541251234"
+              className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline"
+            >
+              (555) 412-1234
+            </a>
+            <a
+              href="#"
+              className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+      </nav>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+          <div className="flex items-center">
+            <ul className="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Company
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Team
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Features
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
